10bf94d5727bf29d825aef12926ab8e2d62b2cd1
Yilan Wang <ylwa@ucdavis.edu>
2024-02-22 12:21:54 -0800

    make an uptade

--- studio2/script.js
+++ studio2/script.js
@@ -1,5 +1,29 @@
-(function(){
+window.addEventListener('load', function(){
     'use strict';
     console.log('reading js');
-    
+
+    const sliderContent = document.querySelector('.a');
+    const sliderWidth = sliderContent.offsetWidth;
+    const cloned = sliderContent.cloneNode(true);
+    cloned.className = "b";
+    document.querySelector('.slider').appendChild(cloned);
+    let root = document.querySelector(':root');
+    const endLeftPos = `-${sliderWidth}px`;    
+    root.style.setProperty('--sliderwidth', endLeftPos);
+    console.log(getComputedStyle(root).getPropertyValue('--sliderwidth'));
+    document.querySelector('.slider').classList.add("animate");
+
+    document.querySelector('.slider').addEventListener('mouseover', function(){
+        document.querySelector('.animate').style.animationPlayState = 'paused';
+    });
+
+    document.querySelector('.slider').addEventListener('mouseout', function(){
+        document.querySelector('.animate').style.animationPlayState = 'running';
+    });
+
+    document.addEventListener('click', function(event){
+        if(event.target.className == 'photo'){
+            console.log(event.target.src);
+        }
+    } );
 })();
